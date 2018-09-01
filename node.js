48abfe43ef6b325522dc65963a3ca57451fefd53
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-09-01 10:33:04 -0300

    add struct add no

--- node.js
+++ node.js
@@ -45,3 +45,17 @@ class Node{
         }
 
     }
+
+
+    let root = new Node(10);
+
+    console.log(root.hasLeftChild());
+    root.add(new Node(7));
+    root.add(new Node(13));
+    root.add(new Node(6));
+    root.add(new Node(12));
+    root.add(new Node(15));
+
+    console.log(root);
+
+    console.log(root.leftChild);    
