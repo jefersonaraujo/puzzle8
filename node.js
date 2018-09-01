697b30dcaad024d895d2b66f00e2a0fdb339dd93
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-09-01 08:13:21 -0300

    add struct function

---
+++ node.js
@@ -0,0 +1,8 @@
+class Node{
+    constructor(key, parent = null, leftChild = null, rightChild = null){
+        this.key = key; 
+        this.parent = parent;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+
+    }
