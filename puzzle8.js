4e536db002578357217e014ba9dcd896620ec773
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-09-04 14:07:54 -0300

    lets go baby

--- puzzle8.js
+++ puzzle8.js
@@ -1,4 +1,33 @@
 var meta =[ ["1","2","3"],
             ["4","5","6"],
-            ["7","8","9"] ];											// estado objetivo, puzzle montado
+            ["7","8","9"] ];											
 
+var estado = copiaEstado(meta);	
+var ultimo = copiaEstado(meta);											// guarda o estado antes da solução
+														// movimentos realizados pelo jogador (humano)
+
+var movimentos = 0;
+var pilha = [];															
+var fechados = [];														
+var nodos = 0;
+var solucao = [];	
+
+
+// copia elementos do array
+function copiaEstado(estado) {											
+	var retorno = [];
+	for (var i = 0; i < estado.length; i++)								
+		retorno[i] = estado[i].slice(0);		
+
+	return retorno;
+}
+
+function exibeEstado(estado) {		// exibe estado na tela
+	for (var i=0; i<3; i++)
+		for (var j=0; j<3; j++) {
+			console.log("p"+i+j);			
+			console.log("c"+estado[i][j]);
+			console.log(estado[i][j]);
+		}
+}
+//exibeEstado()
